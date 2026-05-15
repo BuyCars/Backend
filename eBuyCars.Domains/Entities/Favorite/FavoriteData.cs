8ce574c0f842897e18db0fa978023ae3b497f9b6
Your Name <vlad.graur@isa.utm.md>
2026-05-15 04:19:49 +0300

    FavoriteData

---
+++ eBuyCars.Domains/Entities/Favorite/FavoriteData.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BuyCars.Domain.Entities.Favorite
+{
+    public class FavoriteData
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int CarId { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+    }
+}
